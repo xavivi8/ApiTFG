59feb709509763bca4b8981764cc70d0b6ce0bf2
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-03-28 13:01:38 +0100

    IUserDAO.java CHANGES

--- ApiUsuario/src/main/java/com/application/lol/build/ApiUsuario/persistence/IUserDAO.java
+++ ApiUsuario/src/main/java/com/application/lol/build/ApiUsuario/persistence/IUserDAO.java
@@ -3,12 +3,13 @@ package com.application.lol.build.ApiUsuario.persistence;
 import com.application.lol.build.ApiUsuario.entities.User;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IUserDAO {
 
     List<User> findAll();
 
-    User findById(Long id);
+    Optional<User> findById(Long id);
 
     void save(User user);
 
