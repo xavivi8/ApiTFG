45036d73b416203e2f8cd3da6519395f03bf15ad
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-03-28 12:47:04 +0100

    User.java CREATE

---
+++ ApiUsuario/src/main/java/com/application/lol/build/ApiUsuario/entities/User.java
@@ -0,0 +1,30 @@
+package com.application.lol.build.ApiUsuario.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users" )
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user")
+    private int id_user;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "user")
+    private String user;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "image")
+    private byte[] image;
+}
