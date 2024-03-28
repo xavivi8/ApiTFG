344801d563aef1c7b59b5fdb8c2905128ca5b730
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-03-28 13:19:06 +0100

    IUserService.java CREATE

---
+++ ApiUsuario/src/main/java/com/application/lol/build/ApiUsuario/service/IUserService.java
@@ -0,0 +1,19 @@
+package com.application.lol.build.ApiUsuario.service;
+
+import com.application.lol.build.ApiUsuario.entities.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IUserService {
+
+    List<User> findAll();
+
+    Optional<User> findById(Long id);
+
+    void save(User user);
+
+    void deleteById(Long id);
+
+    User findByUserAndPass(String user, String pass);
+}
