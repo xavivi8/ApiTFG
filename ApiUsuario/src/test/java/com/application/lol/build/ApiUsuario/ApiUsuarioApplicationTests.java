24d70dbe019097b6c1c0ff622bfb0c4da5d8418b
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-03-28 12:31:39 +0100

    INITIAL ApiUsuario COMMIT

---
+++ ApiUsuario/src/test/java/com/application/lol/build/ApiUsuario/ApiUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.lol.build.ApiUsuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
