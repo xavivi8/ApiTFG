8095248495ed514e7fc54906fc3c6a0ee81b679c
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-04-10 19:54:39 +0200

    CREATE Spell.java entities

---
+++ api_build/api_build/src/main/java/com/mobabuild/api_build/entities/Spell.java
@@ -0,0 +1,36 @@
+package com.mobabuild.api_build.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "spell" )
+public class Spell {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String email;
+
+    @Column(name = "champion_level")
+    private String champion_level;
+
+    @Column(name = "game_mode")
+    private String game_mode;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "cooldown")
+    private String cooldown;
+
+    @Column(name = "image")
+    private byte[] image;
+}
