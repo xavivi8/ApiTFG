7d39fb7328ada7072fb691813d5481eab6f123d4
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-04-14 20:28:55 +0200

    FavoriteBuildDTO.java CREATE

---
+++ api_build/src/main/java/com/mobabuild/api_build/controller/dto/FavoriteBuildDTO.java
@@ -0,0 +1,15 @@
+package com.mobabuild.api_build.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class FavoriteBuildDTO {
+
+    private Long id;
+}
