51c266d9c746aef1419b58d590114d6da1814a3c
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-04-14 18:40:43 +0200

    IObjectDAO.java CREATE

---
+++ api_build/src/main/java/com/mobabuild/api_build/persistence/IObjectDAO.java
@@ -0,0 +1,18 @@
+package com.mobabuild.api_build.persistence;
+
+
+import com.mobabuild.api_build.entities.Object;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IObjectDAO {
+
+    List<Object> findAll();
+
+    Optional<Object> findById(Long id);
+
+    void save(Object object);
+
+    void deleteById(Long id);
+}
