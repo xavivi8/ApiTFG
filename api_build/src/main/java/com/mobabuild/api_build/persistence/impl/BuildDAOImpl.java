010e38c0b0b3c108099a1b46a114682299083098
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-04-14 19:09:13 +0200

    BuildDAOImpl.java CREATE

---
+++ api_build/src/main/java/com/mobabuild/api_build/persistence/impl/BuildDAOImpl.java
@@ -0,0 +1,38 @@
+package com.mobabuild.api_build.persistence.impl;
+
+import com.mobabuild.api_build.entities.Build;
+import com.mobabuild.api_build.entities.User;
+import com.mobabuild.api_build.persistence.IBuildDAO;
+import com.mobabuild.api_build.repository.BuildRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class BuildDAOImpl implements IBuildDAO {
+
+    @Autowired
+    private BuildRepository buildRepository;
+
+    @Override
+    public List<Build> findAll() {
+        return (List<Build>) buildRepository.findAll();
+    }
+
+    @Override
+    public Optional<Build> findById(Long id) {
+        return buildRepository.findById(id);
+    }
+
+    @Override
+    public void save(Build build) {
+        buildRepository.save(build);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        buildRepository.deleteById(id);
+    }
+}
