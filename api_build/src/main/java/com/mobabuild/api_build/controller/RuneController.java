5725f62fa46d0de78f601decabc070b12e93626f
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-04-14 20:38:19 +0200

    RuneController.java CREATE

---
+++ api_build/src/main/java/com/mobabuild/api_build/controller/RuneController.java
@@ -0,0 +1,14 @@
+package com.mobabuild.api_build.controller;
+
+import com.mobabuild.api_build.service.IRuneService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/rune")
+public class RuneController {
+
+    @Autowired
+    private IRuneService runeService;
+}
