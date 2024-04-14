4edeefb664985200ea6eaf685e84843e1b213c83
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-04-14 20:28:45 +0200

    BuildDTO.java CREATE

---
+++ api_build/src/main/java/com/mobabuild/api_build/controller/dto/BuildDTO.java
@@ -0,0 +1,16 @@
+package com.mobabuild.api_build.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BuildDTO {
+
+    private Long id;
+    private String buildName;
+}
