076d170922a4ff58ea0e27a20d0555aab00c2e1d
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-04-14 17:45:27 +0200

    ObjectSet.java CREATE

---
+++ api_build/api_build/src/main/java/com/mobabuild/api_build/entities/ObjectSet.java
@@ -0,0 +1,36 @@
+package com.mobabuild.api_build.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "object_set" )
+public class ObjectSet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "build", nullable = false)
+    private Build build;
+
+    @ManyToMany
+    @JoinTable(
+            name = "object_set_object",
+            joinColumns = @JoinColumn(name = "object_set_id"),
+            inverseJoinColumns = @JoinColumn(name = "object_id")
+    )
+    private List<Spell> objects = new ArrayList<>();
+}
