d4cb916f0a4d2628a6b1d77fce3178f83c803ea1
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-04-14 19:18:28 +0200

    IObjectService.java CREATE

---
+++ api_build/src/main/java/com/mobabuild/api_build/service/IObjectService.java
@@ -0,0 +1,17 @@
+package com.mobabuild.api_build.service;
+
+import com.mobabuild.api_build.entities.Object;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IObjectService {
+
+    List<Object> findAll();
+
+    Optional<Object> findById(Long id);
+
+    void save(Object object);
+
+    void deleteById(Long id);
+}
