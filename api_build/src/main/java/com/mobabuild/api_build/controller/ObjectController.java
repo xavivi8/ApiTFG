5f737ae9249dd04ebbc2a85a8a6eac34f48285e8
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-05-04 20:18:31 +0200

    ObjectController.java ADD mapping

--- api_build/src/main/java/com/mobabuild/api_build/controller/ObjectController.java
+++ api_build/src/main/java/com/mobabuild/api_build/controller/ObjectController.java
@@ -1,9 +1,16 @@
 package com.mobabuild.api_build.controller;
 
+import com.mobabuild.api_build.controller.dto.ObjectDTO;
+import com.mobabuild.api_build.entities.Object;
 import com.mobabuild.api_build.service.IObjectService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/object")
@@ -11,4 +18,35 @@ public class ObjectController {
 
     @Autowired
     private IObjectService objectService;
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<?> findById(@PathVariable Long id){
+        Optional<Object> objectOptional = objectService.findById(id);
+
+        if(objectOptional.isPresent()){
+            Object object = objectOptional.get();
+
+            ObjectDTO objectDTO = ObjectDTO.builder()
+                    .id(object.getId())
+                    .name(object.getName())
+                    .objectSets(object.getObjectSets())
+                    .build();
+
+            return ResponseEntity.ok(objectDTO);
+        }
+
+        return  ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/setObject/{name}")
+    public ResponseEntity<?> setObject(@PathVariable String name){
+        int response = objectService.setObject(name);
+
+        if(response == 1){
+
+            return ResponseEntity.ok().build();
+        }
+
+        return  ResponseEntity.notFound().build();
+    }
 }
