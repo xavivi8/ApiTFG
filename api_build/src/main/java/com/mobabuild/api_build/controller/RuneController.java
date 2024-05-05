4dc7d1d4895a450e576f6288995fcc82bab95927
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-05-05 13:14:54 +0200

    ADD CONTROLLER FUNCTIONS

--- api_build/src/main/java/com/mobabuild/api_build/controller/RuneController.java
+++ api_build/src/main/java/com/mobabuild/api_build/controller/RuneController.java
@@ -1,9 +1,20 @@
 package com.mobabuild.api_build.controller;
 
+import com.mobabuild.api_build.controller.dto.ObjectDTO;
+import com.mobabuild.api_build.controller.dto.RuneDTO;
+import com.mobabuild.api_build.entities.Object;
+import com.mobabuild.api_build.entities.Rune;
 import com.mobabuild.api_build.service.IRuneService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/rune")
@@ -11,4 +22,56 @@ public class RuneController {
 
     @Autowired
     private IRuneService runeService;
+
+    @GetMapping("/findById/{id}")
+    public ResponseEntity<?> findById(@PathVariable Long id){
+        Optional<Rune> runeOptional = runeService.findById(id);
+
+        if(runeOptional.isPresent()){
+            Rune rune = runeOptional.get();
+
+            RuneDTO runeDTO = RuneDTO.builder()
+                    .id(rune.getId())
+                    .name(rune.getName())
+                    .group_name(rune.getGroup_name())
+                    .description(rune.getDescription())
+                    .long_description(rune.getLong_description())
+                    .image(rune.getImage())
+                    .build();
+
+            return  ResponseEntity.ok(runeDTO);
+        }
+
+        return  ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/findAll")
+    public ResponseEntity<?> findAll(){
+        List<Rune> runeList = runeService.findAll();
+
+        if(!runeList.isEmpty()){
+            List<RuneDTO> runeDTOList = new ArrayList<>();
+            for(Rune rune : runeList){
+                RuneDTO runeDTO = RuneDTO.builder()
+                        .id(rune.getId())
+                        .name(rune.getName())
+                        .group_name(rune.getGroup_name())
+                        .description(rune.getDescription())
+                        .long_description(rune.getLong_description())
+                        .image(rune.getImage())
+                        .build();
+                runeDTOList.add(runeDTO);
+            }
+            return ResponseEntity.ok(runeDTOList);
+        }
+
+        return  ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/deleteById/{id}")
+    public ResponseEntity<?> deleteById(@PathVariable Long id){
+        runeService.deleteById(id);
+
+        return ResponseEntity.ok("ok");
+    }
 }
--- api_build/src/main/java/com/mobabuild/api_build/controller/SpellController.java
+++ api_build/src/main/java/com/mobabuild/api_build/controller/SpellController.java
@@ -1,9 +1,20 @@
 package com.mobabuild.api_build.controller;
 
+import com.mobabuild.api_build.controller.dto.ObjectDTO;
+import com.mobabuild.api_build.controller.dto.SpellDTO;
+import com.mobabuild.api_build.entities.Object;
+import com.mobabuild.api_build.entities.Spell;
 import com.mobabuild.api_build.service.ISpellService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/spell")
@@ -11,4 +22,60 @@ public class SpellController {
 
     @Autowired
     private ISpellService spellService;
+
+    @GetMapping("/findById/{id}")
+    public ResponseEntity<?> findById(@PathVariable Long id){
+        Optional<Spell> spellOptional = spellService.findById(id);
+
+        if(spellOptional.isPresent()){
+            Spell spell = spellOptional.get();
+
+            SpellDTO spellDTO = SpellDTO.builder()
+                    .id(spell.getId())
+                    .name(spell.getName())
+                    .champion_level(spell.getChampion_level())
+                    .game_mode(spell.getGame_mode())
+                    .description(spell.getDescription())
+                    .cooldown(spell.getCooldown())
+                    .image(spell.getImage())
+                    .spellSets(spell.getSpellSets())
+                    .build();
+
+            return  ResponseEntity.ok(spellDTO);
+        }
+
+        return  ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/findAll")
+    public ResponseEntity<?> findAll(){
+        List<Spell> spellList = spellService.findAll();
+
+        if(!spellList.isEmpty()){
+            List<SpellDTO> spellDTOList = new ArrayList<>();
+            for(Spell spell : spellList){
+                SpellDTO spellDTO = SpellDTO.builder()
+                        .id(spell.getId())
+                        .name(spell.getName())
+                        .champion_level(spell.getChampion_level())
+                        .game_mode(spell.getGame_mode())
+                        .description(spell.getDescription())
+                        .cooldown(spell.getCooldown())
+                        .image(spell.getImage())
+                        .spellSets(spell.getSpellSets())
+                        .build();
+                spellDTOList.add(spellDTO);
+            }
+            return ResponseEntity.ok(spellDTOList);
+        }
+
+        return  ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/deleteById/{id}")
+    public ResponseEntity<?> deleteById(@PathVariable Long id){
+        spellService.deleteById(id);
+
+        return ResponseEntity.ok("ok");
+    }
 }
