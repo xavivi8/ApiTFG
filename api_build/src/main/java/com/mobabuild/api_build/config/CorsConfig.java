886509b9f11c2b390b3b427d9868cc8672851a59
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-05-17 17:51:10 +0200

    CorsConfig.java COMMENT

--- api_build/src/main/java/com/mobabuild/api_build/config/CorsConfig.java
+++ api_build/src/main/java/com/mobabuild/api_build/config/CorsConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
-
+    //Configuración para pruebas locales
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
