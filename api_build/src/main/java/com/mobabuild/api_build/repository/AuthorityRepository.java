122cd6a453a8945d4c04ff48f03906838ce9271d
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-05-23 19:26:00 +0200

    AuthorityRepository.java ADD findAll

--- api_build/src/main/java/com/mobabuild/api_build/repository/AuthorityRepository.java
+++ api_build/src/main/java/com/mobabuild/api_build/repository/AuthorityRepository.java
@@ -12,4 +12,7 @@ import java.util.Optional;
 public interface AuthorityRepository extends CrudRepository<Authority, Long>{
 
     Optional<Authority> findByName(AuthorityName name);
+
+    @Override
+    Iterable<Authority> findAll();
 }
