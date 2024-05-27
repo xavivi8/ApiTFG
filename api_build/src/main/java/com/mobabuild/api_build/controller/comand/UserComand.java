24ad90972cfeb9db46e72dc9419bfc68df0a31b2
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-05-27 21:56:30 +0200

    UserComand.java CHANGES

--- api_build/src/main/java/com/mobabuild/api_build/controller/comand/UserComand.java
+++ api_build/src/main/java/com/mobabuild/api_build/controller/comand/UserComand.java
@@ -3,15 +3,14 @@ package com.mobabuild.api_build.controller.comand;
 import com.mobabuild.api_build.entities.Authority;
 import com.mobabuild.api_build.entities.Build;
 import com.mobabuild.api_build.entities.FavoriteBuild;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
