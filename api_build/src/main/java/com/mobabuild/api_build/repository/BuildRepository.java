5eca244c63804e8e24e4c38f483b4205174f792f
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-05-28 19:44:17 +0200

    BuildRepository.java ADD findByChampionsId

--- api_build/src/main/java/com/mobabuild/api_build/repository/BuildRepository.java
+++ api_build/src/main/java/com/mobabuild/api_build/repository/BuildRepository.java
@@ -1,9 +1,16 @@
 package com.mobabuild.api_build.repository;
 
 import com.mobabuild.api_build.entities.Build;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BuildRepository extends CrudRepository<Build, Long> {
+
+    @Query("SELECT b FROM Build b WHERE b.champions.id = ?1")
+    List<Build> findByChampionsId(Long championsId);
+
 }
