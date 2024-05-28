696d18b699a451f7c7940a54453adaeb4bd2afb5
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-05-28 16:49:17 +0200

    ObjectDTO.java ADD getters AND setters

--- api_build/src/main/java/com/mobabuild/api_build/controller/dto/ObjectDTO.java
+++ api_build/src/main/java/com/mobabuild/api_build/controller/dto/ObjectDTO.java
@@ -1,15 +1,14 @@
 package com.mobabuild.api_build.controller.dto;
 
 import com.mobabuild.api_build.entities.ObjectSet;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
