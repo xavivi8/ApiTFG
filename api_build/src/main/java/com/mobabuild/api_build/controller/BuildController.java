8c96b3769bece60f19ecd0d18e110688b382555c
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-05-28 16:49:02 +0200

    BuildController.java CHANGE

--- api_build/src/main/java/com/mobabuild/api_build/controller/BuildController.java
+++ api_build/src/main/java/com/mobabuild/api_build/controller/BuildController.java
@@ -1,10 +1,11 @@
 package com.mobabuild.api_build.controller;
 
 import com.mobabuild.api_build.controller.comand.BuildComand;
+import com.mobabuild.api_build.controller.comand.ObjectComand;
+import com.mobabuild.api_build.controller.comand.SpellComand;
 import com.mobabuild.api_build.controller.dto.*;
-import com.mobabuild.api_build.entities.Build;
-import com.mobabuild.api_build.entities.Spell;
-import com.mobabuild.api_build.entities.User;
+import com.mobabuild.api_build.entities.*;
+import com.mobabuild.api_build.entities.Object;
 import com.mobabuild.api_build.service.IBuildService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -27,14 +28,7 @@ public class BuildController {
     @PostMapping("/create")
     public ResponseEntity<?> create(@RequestBody BuildComand buildComand){
         try {
-            Build newBuild = Build.builder()
-                    .buildName(buildComand.getBuildName())
-                    .user(buildComand.getUser())
-                    .champions(buildComand.getChampions())
-                    .spellSets(buildComand.getSpellSets())
-                    .objectSet(buildComand.getObjectSet())
-                    .runeSet(buildComand.getRuneSet())
-                    .build();
+            Build newBuild = convertToBuild(buildComand);
 
             buildService.save(newBuild);
 
@@ -57,13 +51,80 @@ public class BuildController {
                                 // Agrega más campos según sea necesario
                                 .build())
                     .collect(Collectors.toList()))
-                    .runeSet(buildComand.getRuneSet())
+                    .runeSet(buildComand.getRuneSet().stream()
+                            .map(runeSet -> RuneSetDTO.builder()
+                                    .id(runeSet.getId())
+                                    .name(runeSet.getName())
+                                    .build())
+                            .collect(Collectors.toList()))
                     .build();
 
             return ResponseEntity.ok(buildDTO);
         } catch (Exception e) {
             return ResponseEntity.status(500).body("Error al añadir la build: " + e.getMessage());
         }
+    }
+
+    private Build convertToBuild(BuildComand buildComand){
+        List<SpellSet> spellSets = buildComand.getSpellSets().stream()
+                .map(spellSet -> SpellSet.builder()
+                        .id(spellSet.getId())
+                        .name(spellSet.getName())
+                        .spells(spellSet.getSpells().stream()
+                                .map(this::convertToSpell)
+                                .collect(Collectors.toList()))
+                        .build())
+                .collect(Collectors.toList());
+
+        List<ObjectSet> objectSets = buildComand.getObjectSet().stream()
+                .map(objectSet -> ObjectSet.builder()
+                        .id(objectSet.getId())
+                        .name(objectSet.getName())
+                        .objects(objectSet.getObjects().stream()
+                                .map(this::convertToObject)
+                                .collect(Collectors.toList()))
+                        .build())
+                .collect(Collectors.toList());
+
+        List<RuneSet> runeSets = buildComand.getRuneSet().stream()
+                .map(runeSet -> RuneSet.builder()
+                        .id(runeSet.getId())
+                        .name(runeSet.getName())
+                        .main_rune(runeSet.getMain_rune())
+                        .main_sub_rune(runeSet.getMain_sub_rune())
+                        .secondary_rune(runeSet.getSecondary_rune())
+                        .secondary_sub_rune(runeSet.getSecondary_sub_rune())
+                        .additional_advantages(runeSet.getAdditional_advantages())
+                        .build())
+                .collect(Collectors.toList());
+
+        return Build.builder()
+                .buildName(buildComand.getBuildName())
+                .user(buildComand.getUser())
+                .champions(buildComand.getChampions())
+                .spellSets(spellSets)
+                .objectSet(objectSets)
+                .runeSet(runeSets)
+                .build();
+    }
+
+    private Spell convertToSpell(SpellComand spellComand) {
+        return Spell.builder()
+                .id(spellComand.getId())
+                .name(spellComand.getName())
+                .champion_level(spellComand.getChampion_level())
+                .game_mode(spellComand.getGame_mode())
+                .description(spellComand.getDescription())
+                .cooldown(spellComand.getCooldown())
+                .image(spellComand.getImage())
+                .build();
+    }
+
+    private Object convertToObject(ObjectComand objectComand) {
+        return Object.builder()
+                .id(objectComand.getId())
+                .name(objectComand.getName())
+                .build();
     }
 
     private UserDTO convertToUserDTO(User user) {
