d4ffad51950590b9ff91f1fc65c50ad7d96a1cdc
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-06-03 17:55:03 +0200

    BlobUtils.java CONTROL null

--- api_build/src/main/java/com/mobabuild/api_build/utils/BlobUtils.java
+++ api_build/src/main/java/com/mobabuild/api_build/utils/BlobUtils.java
@@ -30,6 +30,9 @@ public class BlobUtils {
     }
 
     public static Blob bytesToBlob(byte[] bytes) {
+        if (bytes == null) {
+            return null;
+        }
         try {
             return new SerialBlob(bytes);
         } catch (SQLException e) {
