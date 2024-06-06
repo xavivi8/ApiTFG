c8d298e6f82919082d95ea35615c0cc2487292fb
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-06-06 12:48:46 +0200

    CHANGES Build -> BuildComand

--- api_build/src/main/java/com/mobabuild/api_build/controller/comand/ChampionComand.java
+++ api_build/src/main/java/com/mobabuild/api_build/controller/comand/ChampionComand.java
@@ -1,5 +1,6 @@
 package com.mobabuild.api_build.controller.comand;
 
+import com.mobabuild.api_build.controller.dto.BuildDTO;
 import com.mobabuild.api_build.entities.Build;
 import jakarta.persistence.Column;
 import lombok.*;
@@ -17,5 +18,5 @@ public class ChampionComand {
     private Long id;
     private String name;
     private byte[] image;
-    private List<Build> builds;
+    private List<BuildComand> builds;
 }
